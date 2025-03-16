236d8e7b831a66e7a1ffaaa9ee866eb02f026a41
XIUXIU25 <yingxiuxiu8@gmail.com>
2025-03-15 22:20:27 -0400

    initial my store project

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "hsl(var(--background))",  // Register the background color
+        foreground: "hsl(var(--foreground))",  // Register foreground color
+        border: "hsl(var(--border))",  // Register border color
+      },
+    },
+  },
+  plugins: [],
+};
